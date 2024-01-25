201dfe31924d42b6d132f084c0310591ce28d370
josephErian <gozefatef1999@gmail.com>
2024-01-25 16:05:59 +0200

    add landingpage

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-b padding-b">
+        Hero
+      </section>
+      <section className="padding">
+        Popular Products
+      </section>
+      <section className="padding">
+        SuperQuality
+      </section>
+      <section className="padding-x py-10">
+        Services
+      </section>
+      <section className="padding">
+        Special offers
+      </section>
+      <section className="bg-pale-blue">
+        Customer Reviews
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        Subscribe
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
+  )
+}
+
+export default App
